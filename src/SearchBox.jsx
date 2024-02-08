248f2af16e305824b9e129cee2023b595d024d69
Saurabh <136837795+Saurabh9527@users.noreply.github.com>
2024-02-08 13:28:28 +0530

    first commit

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,8 +7,6 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 function SearchBox({updateInfo}) {
 
-    //& "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-
     let [city, setCity] = useState("");
     let[error , setError] = useState(false);
 
@@ -17,7 +15,7 @@ function SearchBox({updateInfo}) {
 
     let getWeatherInfo = async () => {
         try {
-            let res = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);//&units=metric convert unit in readable format
+            let res = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
             let jsonRes = await res.json();
             console.log(jsonRes);
     
@@ -58,7 +56,6 @@ function SearchBox({updateInfo}) {
 
     return (
         <div className='SearchBox'>
-            {/* <h3>Search For The Weather</h3> */}
             <form onSubmit={handleSubmit}>
                 <TextField id="city" label="City Name" variant="outlined" value={city} onChange={handleChange} style={{border:"1px solid black" , borderRadius:"5px"}} required />
                 <br /><br />
